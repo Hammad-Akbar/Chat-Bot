b61250870dd53b1e71a0023a7a096fa88b1991a4
Sresht Rengesh <sresht@gmail.com>
2020-10-02 00:15:20 +0000

    Convert Button to function

--- scripts/Button.jsx
+++ scripts/Button.jsx
@@ -1,31 +1,21 @@
 import * as React from 'react';
-
 import { Socket } from './Socket';
 
-export class Button extends React.Component {
-    constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    
-    handleSubmit(event) {
-        event.preventDefault();
-    
-        // this is a local variable so we don't need to initialize in the constructor
+export function Button() {
+    const handleSubmit = event => {
         let random = Math.floor(Math.random() * 100);
         console.log('Generated a random number: ', random);
         Socket.emit('new number', {
             'number': random,
         });
         console.log('Sent a random number to server!');
-    }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <button>Send up a random number!</button>
-            </form>
-        );
+        event.preventDefault();
     }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <button>Send up a random number!</button>
+        </form>
+    );
 }
