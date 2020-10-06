5de7171df8a7f4f49f63d7138231c53e6a799766
Kandarp Ajvalia <kandarpa9@gmail.com>
2020-10-06 20:54:16 +0000

    fixed issue where app couldn't access a postgres table

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 # app.py
+from os.path import join, dirname
+from dotenv import load_dotenv
 import os
 import flask
 import flask_sqlalchemy
 
 app = flask.Flask(__name__)
+
+
+dotenv_path = join(dirname(__file__), 'sql.env')
+load_dotenv(dotenv_path)
+
+sql_user = os.environ['SQL_USER']
+sql_pwd = os.environ['SQL_PASSWORD']
+dbuser = os.environ['USER']
+
+database_uri = 'postgresql://{}:{}@localhost/postgres'.format(
+    sql_user, sql_pwd)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+
 db = flask_sqlalchemy.SQLAlchemy(app)
+db.app = app
 
-import models  # this needs to be here
-# notice the SQL_ALCHEMY_CONFIG is removed.
-
+import models 
 
 @app.route('/')
 def index():
