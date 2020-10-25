1b698535abe3113932aeb88bff114f920368569f
EC2 Default User <ec2-user@ip-172-31-67-90.ec2.internal>
2020-10-25 16:49:01 +0000

    Push initial files

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -12,36 +12,17 @@ function check(message) {
     if (message.startsWith(" ")) {
         return 1;
     }
-    /*
-    else if (message.startsWith("!! ")) {
-        return 0;
-    }
-    */
+
     else {
         return 0;
     }
 }
-
-function checkNot(message) {
-    if (message.startsWith(" ") == false) {
-        return message;
-    }
-    else if (message.startsWith("!! ")) {
-        return message;
-    }
-    else {
-        message = null;
-        return message;
-    }
-}
-
 
 export function Content() {
     const [messages, setMessages] = React.useState([]);
     const [accounts, setAccounts] = React.useState([]);
     
     function getNewMessages() {
-        
         React.useEffect(() => {
             Socket.on('messages received', (data) => {
                 console.log("Received messages from server: " + data['allmessages']);
